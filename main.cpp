1ef5315a7e8c2b54681bcde0af4ec408784b3e31
Mohammad Sheraj <mdsheraj123@gmail.com>
2023-01-15 10:40:48 +0530

    change name. Perfect code.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 /*
-author: Mohammad Sheraj
-email: mdsheraj123@gmail.com
+author: a_natural
 //////////////
 Use long long instead of long always.
 printf("%.2lf", double_value);  // 4.00
@@ -63,7 +62,7 @@ void setup() {
     cout.tie(0);
 #ifdef I_AM_DEBUGGING
     freopen("../input.txt", "r", stdin);
-    freopen("../output.txt", "w", stdout);
+    // freopen("../output.txt", "w", stdout);
     cout << "c++ version is " << __cplusplus << endl;
 #ifdef __clang__
     cout << "clang++ " << __clang_major__ << "." << __clang_minor__ << "." << __clang_patchlevel__ << endl;
@@ -76,27 +75,6 @@ void setup() {
 }
 ///////////////////////////////////////////////////////////////
 
-void addToStack(vector<set<int>>& ordering, int at, vector<bool>& visited, stack<int>& output, set<int>& stackMemory, bool& impossible) {
-    if (impossible) {
-        return;
-    }
-
-    stackMemory.insert(at);
-
-    for (int element : ordering[at]) {
-        if (stackMemory.count(element)) {
-            impossible = true;
-        } else if (!visited[element]) {
-            addToStack(ordering, element, visited, output, stackMemory, impossible);
-        }
-    }
-
-    output.push(at);
-    visited[at] = true;
-
-    stackMemory.erase(at);
-}
-
 int main() {
     setup();
     // int T;
@@ -107,53 +85,76 @@ int main() {
     int n;
     cin >> n;
 
-    vector<string> name(n);
+    vector<int> a(n), b(n);
+
     for (int i = 0; i < n; i++) {
-        cin >> name[i];
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
     }
 
-    bool impossible = false;
+    vector<long long> prefixSum(n);
+    for (int i = 0; i < n; i++) {
+        prefixSum[i] = (long long)a[i] * b[i];
+        if (i >= 1) {
+            prefixSum[i] += prefixSum[i - 1];
+        }
+    }
+    long long answer = prefixSum[n - 1];
 
-    vector<set<int>> ordering(26);
-    for (int i = 1; i < n; i++) {
-        int j = 0;
-        bool foundDiff = false;
-        while (j < name[i].size() && j < name[i - 1].size()) {
-            if (name[i][j] != name[i - 1][j]) {
-                ordering[name[i - 1][j] - 'a'].insert(name[i][j] - 'a');
-                foundDiff = true;
-                break;
+    vector<long long> reversePrefixSum1(n);  // center at i
+    vector<long long> reversePrefixSum2(n);  // center at i+0.5
+
+    for (int center = 0; center < n; center++) {
+        memset(reversePrefixSum1.data(), 0, sizeof(long long) * n);
+        memset(reversePrefixSum2.data(), 0, sizeof(long long) * n);
+
+        for (int distance = 0; center - distance >= 0 && center + distance < n; distance++) {
+            reversePrefixSum1[center - distance] = (long long)a[center + distance] * b[center - distance];
+            reversePrefixSum1[center + distance] = (long long)a[center - distance] * b[center + distance];
+        }
+        for (int j = 1; j < n; j++) {
+            reversePrefixSum1[j] += reversePrefixSum1[j - 1];
+        }
+        //////////////
+        for (int distance = 1; center - distance + 1 >= 0 && center + distance < n; distance++) {
+            reversePrefixSum2[center - distance + 1] = (long long)a[center + distance] * b[center - distance + 1];
+            reversePrefixSum2[center + distance] = (long long)a[center - distance + 1] * b[center + distance];
+        }
+        for (int j = 1; j < n; j++) {
+            reversePrefixSum2[j] += reversePrefixSum2[j - 1];
+        }
+
+        //////////////
+
+        for (int distance = 1; distance < n; distance++) {
+            long long sum = 0;
+            if (center - distance >= 0 && center + distance < n) {
+                if (center - distance == 0) {
+                    sum += reversePrefixSum1[center + distance];
+                } else {
+                    sum += prefixSum[center - distance - 1];
+                    sum += reversePrefixSum1[center + distance] - reversePrefixSum1[center - distance - 1];
+                }
+                sum += prefixSum[n - 1] - prefixSum[center + distance];
+                answer = max(answer, sum);
             }
-            j++;
-        }
-        if (!foundDiff && name[i].size() < name[i - 1].size()) {
-            impossible = true;
-        }
-    }
-
-    if (!impossible) {
-        vector<bool> visited(26, false);
-        stack<int> output;
-        set<int> stackMemory;
-
-        for (int i = 0; i < 26; i++) {
-            if (!visited[i]) {
-                addToStack(ordering, i, visited, output, stackMemory, impossible);
+            sum = 0;
+            if (center - distance + 1 >= 0 && center + distance < n) {
+                if (center - distance + 1 == 0) {
+                    sum += reversePrefixSum2[center + distance];
+                } else {
+                    sum += prefixSum[center - distance];
+                    sum += reversePrefixSum2[center + distance] - reversePrefixSum2[center - distance];
+                }
+                sum += prefixSum[n - 1] - prefixSum[center + distance];
+                answer = max(answer, sum);
             }
         }
-
-        if (!impossible) {
-            while (!output.empty()) {
-                cout << (char)(output.top() + 'a');
-                output.pop();
-            }
-            cout << endl;
-        }
     }
 
-    if (impossible) {
-        cout << "Impossible" << endl;
-    }
+    cout << answer << endl;
 
     return 0;
 }
