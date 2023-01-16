4a845825aaf66a0b4375c6b049fec7c9820c9382
Mohammad Sheraj <mdsheraj123@gmail.com>
2023-01-16 12:13:02 +0530

    added ctest

--- main.cpp
+++ main.cpp
@@ -62,7 +62,7 @@ void setup() {
     cout.tie(0);
 #ifdef I_AM_DEBUGGING
     freopen("../input.txt", "r", stdin);
-    // freopen("../output.txt", "w", stdout);
+    freopen("../output.txt", "w", stdout);
     cout << "c++ version is " << __cplusplus << endl;
 #ifdef __clang__
     cout << "clang++ " << __clang_major__ << "." << __clang_minor__ << "." << __clang_patchlevel__ << endl;
@@ -82,79 +82,7 @@ int main() {
     // for (int test_case = 1; test_case <= T; test_case++) {
     // }
 
-    int n;
-    cin >> n;
-
-    vector<int> a(n), b(n);
-
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    for (int i = 0; i < n; i++) {
-        cin >> b[i];
-    }
-
-    vector<long long> prefixSum(n);
-    for (int i = 0; i < n; i++) {
-        prefixSum[i] = (long long)a[i] * b[i];
-        if (i >= 1) {
-            prefixSum[i] += prefixSum[i - 1];
-        }
-    }
-    long long answer = prefixSum[n - 1];
-
-    vector<long long> reversePrefixSum1(n);  // center at i
-    vector<long long> reversePrefixSum2(n);  // center at i+0.5
-
-    for (int center = 0; center < n; center++) {
-        memset(reversePrefixSum1.data(), 0, sizeof(long long) * n);
-        memset(reversePrefixSum2.data(), 0, sizeof(long long) * n);
-
-        for (int distance = 0; center - distance >= 0 && center + distance < n; distance++) {
-            reversePrefixSum1[center - distance] = (long long)a[center + distance] * b[center - distance];
-            reversePrefixSum1[center + distance] = (long long)a[center - distance] * b[center + distance];
-        }
-        for (int j = 1; j < n; j++) {
-            reversePrefixSum1[j] += reversePrefixSum1[j - 1];
-        }
-        //////////////
-        for (int distance = 1; center - distance + 1 >= 0 && center + distance < n; distance++) {
-            reversePrefixSum2[center - distance + 1] = (long long)a[center + distance] * b[center - distance + 1];
-            reversePrefixSum2[center + distance] = (long long)a[center - distance + 1] * b[center + distance];
-        }
-        for (int j = 1; j < n; j++) {
-            reversePrefixSum2[j] += reversePrefixSum2[j - 1];
-        }
-
-        //////////////
-
-        for (int distance = 1; distance < n; distance++) {
-            long long sum = 0;
-            if (center - distance >= 0 && center + distance < n) {
-                if (center - distance == 0) {
-                    sum += reversePrefixSum1[center + distance];
-                } else {
-                    sum += prefixSum[center - distance - 1];
-                    sum += reversePrefixSum1[center + distance] - reversePrefixSum1[center - distance - 1];
-                }
-                sum += prefixSum[n - 1] - prefixSum[center + distance];
-                answer = max(answer, sum);
-            }
-            sum = 0;
-            if (center - distance + 1 >= 0 && center + distance < n) {
-                if (center - distance + 1 == 0) {
-                    sum += reversePrefixSum2[center + distance];
-                } else {
-                    sum += prefixSum[center - distance];
-                    sum += reversePrefixSum2[center + distance] - reversePrefixSum2[center - distance];
-                }
-                sum += prefixSum[n - 1] - prefixSum[center + distance];
-                answer = max(answer, sum);
-            }
-        }
-    }
-
-    cout << answer << endl;
+    cout << 1 << endl;
 
     return 0;
 }
