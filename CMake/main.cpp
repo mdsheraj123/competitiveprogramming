2552b57ebfe13d112296652dc9e6565a3ea906f8
Mohammad Sheraj <mdsheraj123@gmail.com>
2024-03-01 18:36:53 +0530

    Improvement

--- CMake/main.cpp
+++ CMake/main.cpp
@@ -31,7 +31,7 @@ printf("%.2lf", double_value);  // 4.00
 #include <memory>   //unique_ptr/shared_ptr/weak_ptr
 #include <mutex>    //mutex n;n.lock();n.unlock();
 #include <numeric>  //gcd //there will be sqiggly red line but no compilation error
-#include <queue>    //priority_queue <int, vector<int>, greater<int>> minHeap; (priority_queue default is max heap)
+#include <queue>    //priority_queue <int, vector<int>, greater<int>> minHeap; (priority_queue is by default max heap)
 #include <set>      //sorts by value
 #include <stack>
 #include <string>
@@ -79,8 +79,7 @@ void setup(int argc, char* argv[]) {
 // };
 
 /*
-Notes:
-
+NOTES:
 
 
 */
@@ -88,74 +87,10 @@ Notes:
 
 int main(int argc, char* argv[]) {
     setup(argc, argv);
-    int T;
-    cin >> T;
-    for (int test_case = 1; test_case <= T; test_case++) {
-        int n;
-        long long k;
-        cin>>n>>k;
-        vector<long long> a(n), x(n);
-        for(int i=0;i<n;i++) {
-            cin>>a[i];
-        }
-        for(int i=0;i<n;i++) {
-            cin>>x[i];
-        }
-        long long bulletsUsed = 0;
-        int positive = lower_bound(x.begin(),x.end(),0)-x.begin(); // x != 0
-        int negative = positive-1;
-
-        bool possible = true;
-
-        while(possible) {
-
-            bool positivePossible = true;
-            bool negativePossible = true;
-
-            if(positive>=x.size()) {
-                positivePossible = false;
-            }
-            if(negative<0) {
-                negativePossible = false;
-            }
-
-            if(positivePossible && negativePossible) {
-                if(x[positive] <= -x[negative]) {
-                    if(k*x[positive]-bulletsUsed<a[positive]) {
-                        possible = false;
-                    }
-                    bulletsUsed+=a[positive];
-                    positive++;
-                } else {
-                    if(-k*x[negative]-bulletsUsed<a[negative]) {
-                        possible = false;
-                    }
-                    bulletsUsed+=a[negative];
-                    negative--;
-                }
-            } else if(positivePossible) {
-                    if(k*x[positive]-bulletsUsed<a[positive]) {
-                        possible = false;
-                    }
-                    bulletsUsed+=a[positive];
-                    positive++;
-            } else if(negativePossible) {
-                    if(-k*x[negative]-bulletsUsed<a[negative]) {
-                        possible = false;
-                    }
-                    bulletsUsed+=a[negative];
-                    negative--;
-            } else {
-                break;
-            }
-        }
-        if(possible) {
-            cout<<"YES"<<endl;
-        } else {
-            cout<<"NO"<<endl;
-        }
-    }
-
+    // int T;
+    // cin >> T;
+    // for (int test_case = 1; test_case <= T; test_case++) {
+    // }
 
     return 0;
 }
